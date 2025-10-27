6b27184a7c2e5df6b90650e049a842581263d10f
Uriel020 <urielrre@gmail.com>
2025-10-27 01:09:17 -0400

    feat(mapper) add maps to news entity

--- NewsAI.Core/Mappers/NewsProfile.cs
+++ NewsAI.Core/Mappers/NewsProfile.cs
@@ -1,8 +1,15 @@
 using AutoMapper;
+using NewsAI.Core.Entities;
+using NewsAI.Core.Models.News;
 
 namespace NewsAI.Core.Mappers;
 
-public class NewsProfile: Profile
+public class NewsProfile : Profile
 {
-    
+    public NewsProfile()
+    {
+        CreateMap<News, NewsDto>();
+        CreateMap<CreateNewsDTO, News>();
+        CreateMap<UpdateNewsDTO, News>();
+    }
 }
