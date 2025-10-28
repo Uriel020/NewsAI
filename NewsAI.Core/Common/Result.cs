b8ec774ff722f011183e6b60f76121f762caa2bf
Uriel020 <urielrre@gmail.com>
2025-10-27 21:35:47 -0400

    feat(result) add support for validation errors in Result class

--- NewsAI.Core/Common/Result.cs
+++ NewsAI.Core/Common/Result.cs
@@ -1,3 +1,5 @@
+using FluentValidation.Results;
+
 namespace NewsAI.Core.Common;
 
 public class Result<T>
@@ -6,15 +8,22 @@ public class Result<T>
     public bool IsSuccess { get; }
     public string? Error { get; }
 
-    private Result(T value, bool isSuccess, string? error)
+    public List<ValidationFailure>? Errors { get; } = new List<ValidationFailure>();
+
+
+    private Result(T value, bool isSuccess, string? error, List<ValidationFailure>? errors = null)
     {
         Value = value;
         IsSuccess = isSuccess;
         Error = error;
+        Errors = errors;
     }
 
     public static Result<T> Success(T value) => new(value, true, null);
-    
+
     public static Result<T> Failure(string error) => new(default!, false, error);
+
+    public static Result<T> Failure(List<ValidationFailure> errors) => new(default!, false, null, errors);
+
 
 }
