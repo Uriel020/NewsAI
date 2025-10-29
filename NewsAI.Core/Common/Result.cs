d773afd4ed151bc3b9157f314ba3afcdc7eaea57
Uriel020 <urielrre@gmail.com>
2025-10-29 00:59:43 -0400

    fix(Result) enhance Result class to include HttpErrorType in error handling methods

--- NewsAI.Core/Common/Result.cs
+++ NewsAI.Core/Common/Result.cs
@@ -7,14 +7,21 @@ public class Result<T>
     public T Value { get; }
     public bool IsSuccess { get; }
     public string? Error { get; }
-    public List<ValidationFailure>? Errors { get; } = new List<ValidationFailure>();
+    public HttpErrorType HttpErrorType { get; }
+    public List<ValidationFailure>? Errors { get; }
 
 
-    private Result(T value, bool isSuccess, string? error, List<ValidationFailure>? errors = null)
+    private Result(
+        T value,
+        bool isSuccess,
+        string? error,
+        HttpErrorType httpErrorType = HttpErrorType.None,
+        List<ValidationFailure>? errors = null)
     {
         Value = value;
         IsSuccess = isSuccess;
         Error = error;
+        HttpErrorType = httpErrorType;
         Errors = errors;
     }
 
@@ -22,20 +29,20 @@ public class Result<T>
 
     public static Result<T> Failure(string error) => new(default!, false, error);
 
-    public static Result<T> Failure(List<ValidationFailure> errors) => new(default!, false, null, errors);
+    public static Result<T> Failure(List<ValidationFailure> errors) => new(default!, false, null, HttpErrorType.None, errors);
 
 
     //Results for Http Errors
 
-    public static Result<T> BadRequest(string error) => new(default!, false, error); //400 BadRequest
+    public static Result<T> BadRequest(string error) => new(default!, false, error, HttpErrorType.BadRequest); //400 BadRequest
 
-    public static Result<T> Unauthorized(string error) => new(default!, false, error); //401 Unauthorized
+    public static Result<T> Unauthorized(string error = "Invalid access") => new(default!, false, error, HttpErrorType.Unauthorized); //401 Unauthorized
 
-    public static Result<T> Forbidden(string error) => new(default!, false, error); //403 Forbidden 
+    public static Result<T> Forbidden(string error) => new(default!, false, error, HttpErrorType.Forbidden); //403 Forbidden 
 
-    public static Result<T> NotFound(string error) => new(default!, false, error); //404 Not Found
+    public static Result<T> NotFound(string error = "Resource Not Found") => new(default!, false, error, HttpErrorType.NotFound); //404 Not Found
 
-    public static Result<T> Conflict(string error = "Resource Not Found") => new(default!, false, error); //409 Conflict
+    public static Result<T> Conflict(string error) => new(default!, false, error, HttpErrorType.Conflict); //409 Conflict
 
 
 
