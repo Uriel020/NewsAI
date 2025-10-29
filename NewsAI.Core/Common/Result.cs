3bc5c8971c41398f8ee48381435db8a42703bab8
Uriel020 <urielrre@gmail.com>
2025-10-28 22:38:05 -0400

    refactor(Result) remove redundant line and organize HTTP error result methods

--- NewsAI.Core/Common/Result.cs
+++ NewsAI.Core/Common/Result.cs
@@ -7,7 +7,6 @@ public class Result<T>
     public T Value { get; }
     public bool IsSuccess { get; }
     public string? Error { get; }
-
     public List<ValidationFailure>? Errors { get; } = new List<ValidationFailure>();
 
 
@@ -24,6 +23,22 @@ public class Result<T>
     public static Result<T> Failure(string error) => new(default!, false, error);
 
     public static Result<T> Failure(List<ValidationFailure> errors) => new(default!, false, null, errors);
+
+
+    //Results for Http Errors
+
+    public static Result<T> BadRequest(string error) => new(default!, false, error); //400 BadRequest
+
+    public static Result<T> Unauthorized(string error) => new(default!, false, error); //401 Unauthorized
+
+    public static Result<T> Forbidden(string error) => new(default!, false, error); //403 Forbidden 
+
+    public static Result<T> NotFound(string error) => new(default!, false, error); //404 Not Found
+
+    public static Result<T> Conflict(string error = "Resource Not Found") => new(default!, false, error); //409 Conflict
+
+
+
 
 
 }
