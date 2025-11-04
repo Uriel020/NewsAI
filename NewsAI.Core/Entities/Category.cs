822e7abb79f35d16765d0aa3142c63c8113fb7e2
Uriel020 <urielrre@gmail.com>
2025-11-03 21:33:29 -0400

    feat(entities) add Description to category entity

--- NewsAI.Core/Entities/Category.cs
+++ NewsAI.Core/Entities/Category.cs
@@ -6,6 +6,8 @@ public class Category
 
     public string Name { get; set; } = null!;
 
+    public string Description { get; set; } = null!;
+
     public DateTime CreatedAt { get; set; }
 
     public DateTime UpdatedAt { get; set; }
