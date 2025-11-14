1023beaba5ec010c74342f2a54832a4a7a667d4d
Uriel020 <urielrre@gmail.com>
2025-11-13 23:17:39 -0400

    refactor(news) update message in createnewsvalidator

--- NewsAI.Core/Models/News/Validators/CreateNewsValidator.cs
+++ NewsAI.Core/Models/News/Validators/CreateNewsValidator.cs
@@ -8,11 +8,11 @@ public class CreateNewsValidator:AbstractValidator<CreateNewsDto>
     public CreateNewsValidator()
     {
         RuleFor(x => x.Title)
-            .NotEmpty().WithMessage("Title is required")
+            .NotEmpty().WithMessage("News title is required")
             .MaximumLength(200).WithMessage("Title must not exceed 200 characters");
         
         RuleFor(x => x.Description)
-            .NotEmpty().WithMessage("Description is required")
+            .NotEmpty().WithMessage("News description is required")
             .MaximumLength(1000).WithMessage("Description must not exceed 1000 characters");
         
         RuleFor(x => x.Url)
