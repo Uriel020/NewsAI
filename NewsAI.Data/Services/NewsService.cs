fa996d483313276b8c84ea6cb92facdb0767bfc8
Uriel020 <urielrre@gmail.com>
2025-11-15 23:55:57 -0400

    refactor(news) update validateexist method in newsservice

--- NewsAI.Data/Services/NewsService.cs
+++ NewsAI.Data/Services/NewsService.cs
@@ -13,19 +13,19 @@ public class NewsService : INewsService
     private readonly INewsRepository _newsRepository;
     private readonly IValidator<CreateNewsDto> _createNewsValidator;
     private readonly IValidator<UpdateNewsDto> _updateNewsValidator;
-    private readonly IMapper _mapper;
+    private readonly IMapper _newsMapper;
 
     public NewsService(
         INewsRepository newsRepository,
         IValidator<CreateNewsDto> createNewsValidator,
         IValidator<UpdateNewsDto> updateNewsValidator,
-        IMapper mapper
+        IMapper newsMapper
         )
     {
         _newsRepository = newsRepository;
         _createNewsValidator = createNewsValidator;
         _updateNewsValidator = updateNewsValidator;
-        _mapper = mapper;
+        _newsMapper = newsMapper;
     }
 
 
@@ -33,20 +33,18 @@ public class NewsService : INewsService
     {
         var news = await _newsRepository.GetAllAsync();
 
-        var mapNews = _mapper.Map<IEnumerable<NewsDto>>(news);
+        var mapNews = _newsMapper.Map<IEnumerable<NewsDto>>(news);
 
         return Result<IEnumerable<NewsDto>>.Success(mapNews);
     }
 
     public async Task<Result<NewsDto?>> FindById(Guid id)
     {
-        var newExist = await ValidateExist(id);
-
-        if (!newExist) return Result<NewsDto?>.NotFound("News not found with id: " + id);
+        await ValidateExist(id);
 
         var news = await _newsRepository.GetByIdAsync(id);
 
-        var mapNews = _mapper.Map<NewsDto>(news);
+        var mapNews = _newsMapper.Map<NewsDto>(news);
 
         return Result<NewsDto?>.Success(mapNews);
     }
@@ -61,7 +59,7 @@ public class NewsService : INewsService
 
         if (!validateNews.IsValid) return Result<Guid>.Failure(validateNews.Errors);
 
-        var mapNews = _mapper.Map<News>(news);
+        var mapNews = _newsMapper.Map<News>(news);
 
         News newNews = await _newsRepository.AddAsync(mapNews);
 
@@ -84,7 +82,7 @@ public class NewsService : INewsService
 
         if (!validateNews.IsValid) return Result<bool>.Failure(validateNews.Errors);
 
-        var mapNews = _mapper.Map<News>(news);
+        var mapNews = _newsMapper.Map<News>(news);
 
         await _newsRepository.UpdateAsync(mapNews);
 
@@ -94,20 +92,20 @@ public class NewsService : INewsService
 
     public async Task<Result<bool>> Delete(Guid id)
     {
-        var existNew = await ValidateExist(id);
-
-        if (!existNew) return Result<bool>.NotFound("News not found");
+        await ValidateExist(id);
 
         await _newsRepository.DeleteAsync(id);
 
         return Result<bool>.Success(true);
     }
 
-    public async Task<bool> ValidateExist(Guid id)
+    public async Task<Result<bool>> ValidateExist(Guid id)
     {
         var exist = await _newsRepository.GetByIdAsync(id);
 
-        return exist != null;
+        if (exist == null) return Result<bool>.NotFound("News not found");
+
+        return Result<bool>.Success(true);
     }
 
     public Task<Result<bool>> SearchCategory(Guid id)
